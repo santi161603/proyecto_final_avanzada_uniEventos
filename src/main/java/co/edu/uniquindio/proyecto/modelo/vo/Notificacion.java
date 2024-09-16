ad2f69a194d65f99ba0208764dc837e26348a59c
JuanGarcia745 <167032302+JuanGarcia745@users.noreply.github.com>
2024-09-16 18:55:16 -0500

    Se agrega nueva enumeracion

--- src/main/java/co/edu/uniquindio/proyecto/modelo/vo/Notificacion.java
+++ src/main/java/co/edu/uniquindio/proyecto/modelo/vo/Notificacion.java
@@ -1,4 +1,9 @@
 package co.edu.uniquindio.proyecto.modelo.vo;
+import java.util.Date;
 
 public class Notificacion {
+    private String idNotificacion;
+    private String mensaje;
+    private Date fechaEnvio;
+    private boolean leida;
 }
