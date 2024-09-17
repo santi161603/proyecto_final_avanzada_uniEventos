840a702bd708ba03428e063018b7219df6ae105f
santi161603 <116686563+santi161603@users.noreply.github.com>
2024-09-16 19:18:05 -0500

    cambiso

--- src/main/java/co/edu/uniquindio/proyecto/modelo/documentos/Cupon.java
+++ src/main/java/co/edu/uniquindio/proyecto/modelo/documentos/Cupon.java
@@ -5,7 +5,6 @@ import java.util.Date;
 public class Cupon {
     private String codigoCupon;
     private String nombreCupon;
-    private String tipoCupon; // código único, código individual
     private double porcentajeDescuento;
     private Date fechaVencimiento;
 }
