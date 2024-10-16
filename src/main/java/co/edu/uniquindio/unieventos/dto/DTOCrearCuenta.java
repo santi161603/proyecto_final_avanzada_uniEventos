d1a6590c8b18a21520ec6ccacc684d8599ba8206
santi161603 <116686563+santi161603@users.noreply.github.com>
2024-10-16 15:09:00 -0500

    uodate

--- src/main/java/co/edu/uniquindio/unieventos/dto/DTOCrearCuenta.java
+++ src/main/java/co/edu/uniquindio/unieventos/dto/DTOCrearCuenta.java
@@ -3,6 +3,7 @@ package co.edu.uniquindio.unieventos.dto;
 import co.edu.uniquindio.unieventos.modelo.enums.RolUsuario;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import org.hibernate.validator.constraints.Length;
 
@@ -11,10 +12,11 @@ import java.util.List;
 public record DTOCrearCuenta(
         @NotBlank @Length(max = 10) String cedula,
         @NotBlank @Length(max = 100) String nombre,
-        @NotBlank @Length(max = 10) List<String> telefono,
+        @NotEmpty(message = "La lista de teléfonos no puede estar vacía")
+        List<@NotBlank(message = "El número de teléfono no puede estar en blanco") @Length(max = 10) String> telefono,
         @Length(max = 100) String direccion,
         @NotBlank @Length(max = 50) @Email String email,
         @NotBlank @Length(min = 7, max = 20) String contrasena,
-        @NotNull RolUsuario rol  // O puedes cambiarlo por un tipo específico de RolUsuario
+        @NotNull RolUsuario rol
 ) {
 }
