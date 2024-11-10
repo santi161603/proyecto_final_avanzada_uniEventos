f43893059944bb74d324bbbd1138462af20aafc4
JuanGarcia745 <167032302+JuanGarcia745@users.noreply.github.com>
2024-11-10 17:22:37 -0500

    .

--- src/main/java/co/edu/uniquindio/unieventos/modelo/documentos/LocalidadEvento.java
+++ src/main/java/co/edu/uniquindio/unieventos/modelo/documentos/LocalidadEvento.java
@@ -18,7 +18,6 @@ public class LocalidadEvento {
     @Id
     @EqualsAndHashCode.Include
     private String IdLocalidad;
-
     private String nombreLocalidad;
     private String direccion;
     private Ciudades ciudad;
